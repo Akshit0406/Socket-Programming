e6b1d285174093633ec9cb4cb8a745682d6dfa5a
Akshit Pandharkar <128708109+Akshit0604@users.noreply.github.com>
2023-03-26 15:35:49 +0530

    Add files via upload

---
+++ client2.py
@@ -0,0 +1,28 @@
+import time, socket, sys
+
+print("\nWelcome to Head to Head PL Draft\n")
+time.sleep(1)
+
+s= socket.socket()
+shost = socket.gethostname()
+ip = socket.gethostbyname(shost)
+host= input(str("Enter Group id: "))
+port= 1234
+s.connect((host, port))
+
+while True:
+    buffer= s.recv(1024) #receiving message from server
+    buffer= buffer.decode() #decoding send message
+    print(buffer) #printing on client's screen
+    buffer= input(str("")) #selecting player 
+    s.send(buffer.encode()) #sending message to server
+    if(buffer=="End of draft\n"):
+    	break
+    
+    
+while True:
+	buffer= s.recv(1024) #receiving message from server
+	buffer= buffer.decode() #decoding send message
+	print(buffer) #printing on client's screen
+	if(buffer=="Your results will be conveyed shortly!"):
+		break
