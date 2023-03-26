dbe9b13fd671e0faf4b5d6b2ec53d12ea271af68
Akshit Pandharkar <128708109+Akshit0604@users.noreply.github.com>
2023-03-26 15:34:42 +0530

    Update server.py

--- server.py
+++ server.py
@@ -10,9 +10,9 @@ port = 1234
 s.bind((host, port))
 print(host, "(", ip, ")\n")
            
-s.listen(1)
-client, addr = s.accept()
-
+s.listen(2)
+client1, addr = s.accept()
+client2, addr= s.accept()
 teamone=[]
 teamtwo=[]
 
@@ -20,8 +20,8 @@ for i in range(10):
     
 	if(i==0 or i==3 or i==4 or i==7 or i==8):
 		buffer= "Player1 enter choice"
-		client.send(buffer.encode())
-		buffer= client.recv(1024)
+		client1.send(buffer.encode())
+		buffer= client1.recv(1024)
 		buffer= buffer.decode()
 		
 		while True:
@@ -30,12 +30,12 @@ for i in range(10):
 				break
 			else:
 				buffer="Entered player is invalid or not in the top picks."
-				client.send(buffer.encode())	 
+				client1.send(buffer.encode())	 
 	
 	else:
 		buffer= "Player two enter choice"
-		client.send(buffer.encode())
-		buffer= client.recv(1024)
+		client2.send(buffer.encode())
+		buffer= client2.recv(1024)
 		buffer= buffer.decode()
 		while True:
 			if buffer in vp.df['web_name'].values:
@@ -43,32 +43,43 @@ for i in range(10):
 				break
 			else:
 				buffer="Entered player is invalid or not in the top picks."
-				client.send(buffer.encode())	 
+				client2.send(buffer.encode())	 
 	
 		
 		
 
 buffer="End of draft\n"
-client.send(buffer.encode())
+client1.send(buffer.encode())
+client2.send(buffer.encode())
 
 buffer="TEAM ONE:\n" #sending team one
-client.send(buffer.encode())
+client1.send(buffer.encode())
+client2.send(buffer.encode())
+
 for i in range(5):
 	buffer=teamone[i]
-	client.send(buffer.encode())
+	client1.send(buffer.encode())
+	client2.send(buffer.encode())
 	buffer="\n"
-	client.send(buffer.encode())
+	client1.send(buffer.encode())
+	client2.send(buffer.encode())
 	
+		
 buffer="TEAM TWO:\n" #sending team two
-client.send(buffer.encode()) 
+client1.send(buffer.encode()) 
+client2.send(buffer.encode())
 
 for i in range(5):
 	buffer=(teamtwo[i])
-	client.send(buffer.encode())
+	client1.send(buffer.encode())
+	client2.send(buffer.encode())
 	buffer="\n"
-	client.send(buffer.encode())
-	
+	client1.send(buffer.encode())
+	client2.send(buffer.encode())
+
 buffer="Thanks for playing!\n"
-client.send(buffer.encode())
+client1.send(buffer.encode())
+client2.send(buffer.encode())
 buffer="Your results will be conveyed shortly!"
-client.send(buffer.encode())
+client1.send(buffer.encode())
+client2.send(buffer.encode())
