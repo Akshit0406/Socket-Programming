7359d3c3fcecb505870516568e98725c2230d1d0
Akshit Pandharkar <128708109+Akshit0604@users.noreply.github.com>
2023-03-26 15:35:28 +0530

    Update Valid_Players.py

--- Valid_Players.py
+++ Valid_Players.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
 # Reading a CSV file into a pandas DataFrame
-df = pd.read_csv('players.csv')
+df= pd.read_csv('players.csv')
 
 #cleaning the data
 #Removing all columns other than web_name, total_points and position
-df = df[['web_name','total_points','position']]
+df= df[['web_name','total_points','position']]
 #sorting with 2 params
 df = df.sort_values(['position', 'total_points'], ascending=[True, False])
 
@@ -14,8 +14,10 @@ top_fwd = df.groupby('position').get_group('FWD').head(10)
 top_gk = df.groupby('position').get_group('GKP').head(10)
 top_mid = df.groupby('position').get_group('MID').head(30)
 
+sn=range(1,71)
 # Concatenate the selected players into a single DataFrame
 df= pd.concat([top_def, top_fwd, top_gk, top_mid])
-
+df['Serial_No']=sn
+df=df[['Serial_No','web_name','total_points','position']]
 #print(df.head(10)) #checking if sorted properly
 #print(df.tail(10))
