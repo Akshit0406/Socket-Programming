5f1d3cd693de28c7fd3a284389fea477d0f44e71
Akshit Pandharkar <128708109+Akshit0604@users.noreply.github.com>
2023-03-30 19:29:57 +0530

    Update client2.py

--- client2.py
+++ client2.py
@@ -1,4 +1,5 @@
 import time, socket, sys
+import Valid_Players as vp
 
 print("\nWelcome to Head to Head PL Draft\n")
 time.sleep(1)
@@ -10,19 +11,30 @@ host= input(str("Enter Group id: "))
 port= 1234
 s.connect((host, port))
 
+print("Here is the list of available players\n")
+print(vp.df.head(35))
+print(vp.df.tail(35))
+
 while True:
-    buffer= s.recv(1024) #receiving message from server
-    buffer= buffer.decode() #decoding send message
-    print(buffer) #printing on client's screen
-    buffer= input(str("")) #selecting player 
-    s.send(buffer.encode()) #sending message to server
-    if(buffer=="End of draft\n"):
-    	break
+	buffer= s.recv(1024) #receiving message from server
+	buffer= buffer.decode() #decoding send message
+	print(buffer) #printing on client's screen
+	buffer= input(str("")) #selecting player
+	s.send(buffer.encode()) #sending message to server
+	while True:
+		if buffer=="You have already picked this player!" or buffer=="Entered player is invalid or not in the top picks." or buffer=="Player 1 has already picked this player!":
+			buffer= input(str("Please pick a new Player: ")) #selecting player 
+			s.send(buffer.encode()) #sending message to server
+		else:
+			break
+			
+	if buffer=="End of draft\n":
+	    	break
     
     
 while True:
 	buffer= s.recv(1024) #receiving message from server
 	buffer= buffer.decode() #decoding send message
 	print(buffer) #printing on client's screen
-	if(buffer=="Your results will be conveyed shortly!"):
+	if buffer=="Your results will be conveyed shortly!":
 		break
